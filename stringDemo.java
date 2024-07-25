154399bbfd689d41e606b3e5ca434e8c9afee746
phoramapon3 <152240902+phoramapon1@users.noreply.github.com>
2024-07-25 14:28:25 +0700

    Create stringDemo.java

---
+++ stringDemo.java
@@ -0,0 +1,9 @@
+public class stringDemo {
+    public static void main(String[] args){
+        String Greeting ;
+        Greeting = "Good morning";
+        String name = "Computer";
+        System.out.println(Greeting+ " " +name);
+        
+    }
+}
