ae6a5896612b582a64fff484b883a5aef7a7cb61
phoramapon3 <152240902+phoramapon1@users.noreply.github.com>
2024-07-31 15:42:48 +0700

    Create TestFor.java

---
+++ TestFor.java
@@ -0,0 +1,8 @@
+public class TestFor {
+    public static void main(String[] args){
+        int L = 2; 
+        for (int i = 1; i <= 12; i++) 
+            System.out.println(L + " x " + i + " = " + (L * i));
+        }
+    }
+
