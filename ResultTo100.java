42d58f6d49247707a3a906545d929bc69246c2bc
phoramapon3 <152240902+phoramapon1@users.noreply.github.com>
2024-08-01 10:08:08 +0700

    Create ResultTo100.java

---
+++ ResultTo100.java
@@ -0,0 +1,14 @@
+public class ResultTo100 {
+    public static void main(String[] args){
+      int r = 1;
+      int total = 0;
+      while (r <= 100) {
+        System.out.println("result in whileloop"+r);
+        total += r;
+        r++;
+        }
+      System.out.println("Sum of 1 to 100 is: " + total);
+     }
+    }
+ 
+
