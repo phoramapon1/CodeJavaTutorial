38bdf49e4679d907a97403d179561a5af61c4ea6
phoramapon3 <152240902+phoramapon1@users.noreply.github.com>
2024-08-01 14:20:32 +0700

    Create ExNo3.java

---
+++ ExNo3.java
@@ -0,0 +1,13 @@
+public class ExNo3 {
+    public static void main(String[] args) {
+        int number = 8; 
+
+        System.out.println("Multiplication table for " + number + ":");
+
+        for (int k = 1; k <= 10; k++) {
+            
+            System.out.println(number + " x " + k + " = " + (number * k));
+        }
+    }
+}
+        
