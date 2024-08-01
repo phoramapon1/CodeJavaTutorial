2291784cbe131ad863e6bb26bf71b6a3cee9cbe7
phoramapon3 <152240902+phoramapon1@users.noreply.github.com>
2024-08-01 14:20:27 +0700

    Create ExNO1.java

---
+++ ExNO1.java
@@ -0,0 +1,20 @@
+public class ExNO1 {
+    public static void main(String[] args) {
+        int start = 1;
+        int end = 100;
+        System.out.println("Numbers from " + start + " to " + end + ":");
+
+        for (int i = start; i <= end; i++) {
+            
+            if (i % 2 == 0) {
+                System.out.println(i + " ");
+            }
+        }
+
+        System.out.println(); 
+    }
+}
+
+        
+        
+
