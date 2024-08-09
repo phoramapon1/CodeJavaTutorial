4759635f0316808ffd8be72b1ef3a058c1c02593
phoramapon3 <152240902+phoramapon1@users.noreply.github.com>
2024-08-09 13:25:42 +0700

    Create Student1.java

---
+++ Student1.java
@@ -0,0 +1,22 @@
+public class Student1 {
+    private String name;
+    private String email;
+
+    public Student1(){
+        name = "Unassigned";
+        email = "Unassigned";
+    }
+    public void setName(String Student1Name){
+        name = Student1Name;
+    }
+    public void setEmail(String Adress){
+        name = Adress;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+
+}
